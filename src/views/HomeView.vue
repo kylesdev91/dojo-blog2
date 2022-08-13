6b387a2f19bc90668de72657c701db71bcd73844
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-13 16:31:27 -0500

    75 and 76

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,47 @@
-
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
-    <button @click="updateNinjaTwo">Update nina two</button>
+    <input type="text" v-model="search" />
+    <p>search term: {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue';
+
 export default {
   name: 'Home',
-  setup() {    
-    const ninjaOne = ref({ name: 'Kyle', age: 16 })
-    const ninjaTwo = reactive({ name: 'Kate', age: 13})
+  setup() {
+    const search = ref('');
+    const names = ref([
+      'Alex Carrilo',
+      'Ethan Trevino',
+      'Jeffrey-James Turner',
+      'Kyle Hernandez',
+      'Nixon Blankenship',
+      'Nolan Blankenship',
+    ]);
 
-    const nameOne = ref('Kyle')
-    const nameTwo = reactive('Kate')
+    const stopWatch = watch(search, () => {
+      console.log('watch function ran');
+    });
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 52
-    }
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 48
-    }
-    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameTwo }
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect function ran', search.value);
+    });
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    const handleClick = () => {
+      stopWatch();
+      stopEffect();
+    };
+
+    return { names, search, matchingNames, handleClick };
   },
-}
+};
 </script>
