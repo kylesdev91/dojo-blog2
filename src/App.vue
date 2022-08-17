08ad642277d2129ec912adddadc032d647e2f4e4
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-16 22:05:12 -0500

    86

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
+  <Navbar />
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components: { Navbar }
 }
+</script>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+    text-align: inherit;
+  }
+  header h1 {
+    color: #dfdfdf;
+    font-size: 48px;
+  }
+  header a {
+    color: #bbb;
+    text-decoration: none;
+    margin-left: 20px;
+  }
+  header a.router-link-active {
+    color: #444;
+    font-weight: bold;
+    margin-left: 1000px;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+<header>
+  <div id="nav">
+    <h1>The Dojo Blog</h1>
+     <router-link to="/">Home</router-link>
+     <router-link to="/create">Create</router-link>
+         </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
