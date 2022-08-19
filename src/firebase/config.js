0990b238a88457a8bc4c418ec9ba9a817012eadf
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-19 09:13:02 -0500

    91-93

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAz1JpxXHL6ToGdF55V4-cXs_gSSuGyQA8",
+    authDomain: "udemy-vue-firebase-sites-945d0.firebaseapp.com",
+    projectId: "udemy-vue-firebase-sites-945d0",
+    storageBucket: "udemy-vue-firebase-sites-945d0.appspot.com",
+    messagingSenderId: "767394491671",
+    appId: "1:767394491671:web:b5514ce493e65794459b52"
+  };
+
+  // init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  // init firestore service
+  const projectFirestore = firebase.firestore()
+
+  export { projectFirestore }
